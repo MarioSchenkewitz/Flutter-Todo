ed68f77fe99994e8ececd79b3f976897e0702708
Cirrus CI <support@cirruslabs.org>
2023-04-30 19:12:43 +0000

    added popupmenuitems

--- lib/screens/TodoAddPage.dart
+++ lib/screens/TodoAddPage.dart
@@ -6,7 +6,11 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'dart:io' show Platform;
 
 class AddTodoPage extends StatefulWidget {
-  const AddTodoPage({super.key});
+  final Map? todo;
+  const AddTodoPage({
+    super.key,
+    this.todo,
+  });
 
   @override
   State<AddTodoPage> createState() => _AddTodoPageState();
@@ -14,13 +18,28 @@ class AddTodoPage extends StatefulWidget {
 
 class _AddTodoPageState extends State<AddTodoPage> {
   TextEditingController titleController = TextEditingController();
-  TextEditingController completedController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+  bool done = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['todo'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Neues Todo'),
+        title: Text(isEdit ? 'Edit Todo' : 'Neues Todo'),
       ),
       body: ListView(
         padding: EdgeInsets.all(20),
@@ -37,28 +56,72 @@ class _AddTodoPageState extends State<AddTodoPage> {
       ),
       const SizedBox(height: 20),
       TextField(
-        controller: completedController,
+        controller: descriptionController,
         decoration: const InputDecoration(hintText: 'Beschreibung'),
         keyboardType: TextInputType.multiline,
         minLines: 5,
         maxLines: 8,
       ),
+      Checkbox(
+          value: done,
+          onChanged: (bool? value) {
+            setState(() {
+              done = value!;
+            });
+          }),
       const SizedBox(height: 20),
       ElevatedButton(
-        onPressed: submitData,
-        child: const Text('Abschicken'),
+        onPressed: isEdit ? updateData : submitData,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(isEdit ? 'Ändern' : 'Abschicken'),
+        ),
       )
     ];
   }
 
-  void submitData() async {
+  Future<void> updateData() async {
+    //Get data from form
+    final todo = widget.todo;
+    if (todo == null) {
+      print('cant call update without todo data');
+      return;
+    }
+    final id = todo['id'];
+    //final isCompleted = todo['completed'];
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "todo": title,
+      "completed": done,
+      "userId": 5,
+    };
+
+    //Submit update to server
+    final url = 'https://dummyjson.com/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.put(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    //show success or failure
+    if (response.statusCode == 200) {
+      showSuccessMessage('Bearbeitung Erfolgreich');
+    } else {
+      showErrorMessage('Bearbeitung Fehler');
+    }
+  }
+
+  Future<void> submitData() async {
     //Get data from form
     final title = titleController.text;
-    final description = completedController.text;
+    final description = descriptionController.text;
     final body = {
       "todo": title,
       "completed": false,
-      "userId": _getId(),
+      "userId": 5,
     };
 
     //Submit data to server
@@ -71,17 +134,32 @@ class _AddTodoPageState extends State<AddTodoPage> {
     );
 
     //show success or failure
-    if (response.statusCode == 201) {
-      print('Success');
+    if (response.statusCode == 200) {
+      titleController.text = '';
+      descriptionController.text = '';
       showSuccessMessage('Erfolg');
     } else {
       print('Failed');
-      showSuccessMessage('Fehler');
+      showErrorMessage('Fehler');
     }
   }
 
   void showSuccessMessage(String message) {
-    final snackBar = SnackBar(content: Text(message));
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
--- lib/screens/TodoListPage.dart
+++ lib/screens/TodoListPage.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_vorlesung/screens/TodoAddPage.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -9,11 +13,50 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Taskbuddy'),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator()),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index + 1}')),
+                title: Text(item['todo']),
+                trailing: PopupMenuButton(
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(
+                        child: Text('Bearbeiten'),
+                        value: 'edit',
+                      ),
+                      PopupMenuItem(
+                        child: Text('Loeschen'),
+                        value: 'delete',
+                      ),
+                    ];
+                  },
+                ),
+              );
+            },
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddPage,
@@ -27,5 +70,56 @@ class _TodoListPageState extends State<TodoListPage> {
       builder: (context) => AddTodoPage(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> deleteById(String id) async {
+    //Delete item
+    final url = 'https://dummyjson.com/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+
+    if (response.body == 200) {
+      //refresh list
+      showSuccessMessage('Löschen Erfolgreich');
+      fetchTodo();
+    } else {
+      //error
+      showErrorMessage('Löschen fehlgeschlagen');
+    }
+  }
+
+  Future<void> fetchTodo() async {
+    final response = await http.get(Uri.https('dummyjson.com', 'todos'));
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['todos'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
